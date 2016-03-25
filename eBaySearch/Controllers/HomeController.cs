e3c2bac460c1156ab59652aac9501bee6ac985f5
Brian Walsh <brian.walsh3@mycit.ie>
2016-03-25 20:59:01 +0000

    First Commit

---
+++ eBaySearch/Controllers/HomeController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using eBaySearch.Models;
+using eBaySearch.Finding;
+using eBaySearch.Services.Common;
+using eBaySearch.Services;
+
+
+namespace eBaySearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static string appID = "BrianWal-f6d8-43f7-80f0-854c2a33aded";
+        public static string findingServerAddress = "http://svcs.ebay.com/services/search/FindingService/v1?&sortOrder=PriceShippingLowest";
+        public ClientConfig config = new ClientConfig(appID, findingServerAddress);
+
+        [HttpPost]
+        public ActionResult Index(string Id)
+        {
+
+            config.GlobalId = "EBAY-IE"; //use Irish eBay website (www.eBay.ie)
+            FindingServicePortTypeClient client = FindingServiceClientFactory.getServiceClient(config);
+
+            ViewBag.Message = Id;
+
+            SearchItem[] items = null;
+            try
+            {
+                // Create request object
+                FindItemsAdvancedRequest request = new FindItemsAdvancedRequest();
+
+                //filter items so just "buy it now" items appear
+                ItemFilter itemFilter1 = new ItemFilter();
+                itemFilter1.name = ItemFilterType.ListingType;
+                itemFilter1.value = new string[] { "FixedPrice" };
+
+                ItemFilter[] itemFilterArray = new ItemFilter[1];
+                itemFilterArray[0] = itemFilter1;
+
+                // Set request parameters
+                request.keywords = Id;
+                request.itemFilter = itemFilterArray;
+                request.sortOrder = SortOrderType.PricePlusShippingLowest;
+                //request.sortOrderSpecified = true;
+                OutputSelectorType[] outputs = { OutputSelectorType.SellerInfo };
+                request.outputSelector = outputs;
+                //request.categoryId = new string[] {"#11450"};
+
+
+                PaginationInput PaginationInput = new PaginationInput();
+                //PaginationInput.totalNumberOfPages = 5;
+                PaginationInput.entriesPerPageSpecified = true;
+                PaginationInput.entriesPerPage = 250;
+                PaginationInput.pageNumberSpecified = true;
+                PaginationInput.pageNumber = 10;
+                request.paginationInput = PaginationInput;
+
+                // Call the api
+                FindItemsAdvancedResponse response = client.findItemsAdvanced(request);
+
+                /* using (SqlConnection oConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+                 {
+
+                     SqlCommand cmd = new SqlCommand();
+                     cmd.CommandText = "INSERT INTO Searches(SearchText) VALUES(@SearchText, @PhoneNo, @Address)";
+                     cmd.CommandType = CommandType.Text;
+                     cmd.Parameters.AddWithValue("@SearchText", Id);
+                     cmd.Connection = oConn;
+                 }*/
+                using (SqlConnection oConn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+                {
+                    string insertSearch = "INSERT into searches (SearchTest, NoOfTimesSearched) VALUES (@SearchTest,@NoOfTimesSearched)";
+
+                    using (SqlCommand queryinsertSearch = new SqlCommand(insertSearch))
+                    {
+                        queryinsertSearch.Connection = oConn;
+                        queryinsertSearch.Parameters.Add("@SearchTest", SqlDbType.VarChar, 30).Value = Id;
+                        queryinsertSearch.Parameters.Add("@NoOfTimesSearched", SqlDbType.Int, 30).Value = 1;
+                        oConn.Open();
+                        oConn.Close();
+                    }
+                }
+
+
+                // Show output
+                if (response.searchResult != null && response.searchResult.item != null)
+                {
+                    items = response.searchResult.item;
+                    ViewBag.resultcount = response.searchResult.count;
+                    ViewBag.totalitems = response.paginationOutput.totalEntries;
+
+
+
+                    return View(items);
+                }
+
+                //
+                // noResult.inn
+
+            }
+            catch (Exception ex)
+            {
+                var errorText = ex.Message;
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "TShirt";
+            return View();
+        }
+
+        public ActionResult Users(string Id)
+        {
+            FindItemsByKeywordsRequest req = new FindItemsByKeywordsRequest();
+            FindItemsByKeywordsResponse resp = new FindItemsByKeywordsResponse();
+
+            return View();
+        }
+
+        /* public ActionResult SellerItems(string Id)
+         {
+             FindingServicePortTypeClient client = FindingServiceClientFactory.getServiceClient(config);
+             ViewBag.Seller = Id;
+             SearchItem[] items = null;
+             try
+             {
+                 // Create request object
+               FindItemsAdvancedRequest  request = new FindItemsAdvancedRequest();
+
+                 ItemFilter[] ifilter = { new ItemFilter { name = ItemFilterType.Seller, value = new string[] { Id } } };
+                 request.itemFilter = ifilter;
+
+                 PaginationInput pi = new PaginationInput();
+                 pi.entriesPerPage = 105;
+                 pi.entriesPerPageSpecified = true;
+                 request.paginationInput = pi;
+
+                 // Call the service
+                 FindItemsAdvancedResponse response = client.findItemsAdvanced(request);
+
+                 // Show output
+                 if (response.searchResult != null && response.searchResult.item != null)
+                 {
+                     items = response.searchResult.item;
+                     ViewBag.resultcount = response.searchResult.count;
+                     ViewBag.totalitems = response.paginationOutput.totalEntries;
+                     ViewBag.location = items[0].location;
+                     return View(items);
+                 }
+
+             }
+             catch (Exception ex)
+             {
+                 var errorText = ex.Message;
+             }
+
+             return View();
+         }*/
+    }
+}
---
+++ eBaySearch/Models/CustomFindingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using eBaySearch.Finding;
+using System.Net;
+
+namespace eBaySearch.Models
+{
+    public class CustomFindingService
+    {
+        //protected override WebRequest GetWebRequest(Uri uri)
+        //{
+        //    try
+        //    {
+        //        HttpWebRequest request = (HttpWebRequest)base.(uri);
+        //        request.Headers.Add("X-EBAY-SOA-SECURITY-APPNAME", "BrianWal-f6d8-43f7-80f0-854c2a33aded");
+        //        request.Headers.Add("X-EBAY-SOA-OPERATION-NAME", "findItemsByKeywords");
+        //        request.Headers.Add("X-EBAY-SOA-SERVICE-NAME", "FindingService");
+        //        request.Headers.Add("X-EBAY-SOA-MESSAGE-PROTOCOL", "SOAP11");
+        //        request.Headers.Add("X-EBAY-SOA-SERVICE-VERSION", "1.0.0");
+        //        request.Headers.Add("X-EBAY-SOA-GLOBAL-ID", "EBAY-IE");
+        //        return request;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+    }
+}
